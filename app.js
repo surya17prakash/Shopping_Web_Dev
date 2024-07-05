470142b1941b0218ba83a5b7f2c632ddd90f5e88
surya17prakash <surya17prakash@gmail.com>
2024-07-05 12:53:12 +0530

    intialize all base MVC files and folders

---
+++ app.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const authRoutes=require('./routes/auth.routes');
+const app = express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(authRoutes);
+app.listen(3000);
