e4553698cc359bc8d08a484d1688a9828ef59162
surya17prakash <surya17prakash@gmail.com>
2024-07-28 12:18:22 +0530

    list products mvc

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,19 +1,51 @@
-function getProducts(req,res){
-    res.render('admin/products/all-products');
+const Product=require('../models/product.model');
+async function getProducts(req,res,next){
+    try{
+    const products = await Product.findAll();
+    res.render('admin/products/all-products',{products:products});
+    }catch(error){
+        next(error);
+        return;
+    }
+    
 }
 
 function getNewProduct(req,res){
     res.render('admin/products/new-product');
 }
 
-function createNewProduct(req,res){
-   console.log(req.body);
-   console.log(req.file); 
+async function createNewProduct(req,res,next){
+  const product=new Product({
+   ...req.body,
+    image:req.file.filename,
+  });
+  try{ 
+  await product.save();
+  }catch(error){
+    next(error);
+    return;
+  }
    res.redirect('/admin/products');
+}
+
+async function getUpdateProduct(req,res){
+  try{
+ const product=await Product.findById(req.params.id);
+ res.render('admin/products/update-product',{product:product});
+  }catch(error){
+   next(error);
+   return; 
+  }
+}
+
+function updateProduct(){
+
 }
 
 module.exports={
     getProducts:getProducts,
     getNewProduct:getNewProduct,
     createNewProduct:createNewProduct,
+    getUpdateProduct:getUpdateProduct,
+    updateProduct:updateProduct,
 }
---
+++ models/product.model.js
@@ -0,0 +1,51 @@
+const db=require('../data/database');
+const mongodb=require('mongodb');
+class Product{
+    constructor(productData){
+        this.title=productData.title;
+        this.summary=productData.summary;
+        this.price= +productData.price;
+        this.description=productData.description;
+        this.image=productData.image;
+        this.imagePath=`product-data/images/${productData.image}`;
+        this.imageUrl= `/product/assets/images/${productData.image}`;
+        if(productData._id){
+        this.id=productData._id.toString();
+        }
+    }
+    static async findById(productId){
+        let prodId;
+        try{
+        const prodId=new mongodb.ObjectId(productId);
+        }catch(error){
+            error.code=404;
+            throw error;
+        }
+        const product=await db.getDb().collection('products').findOne({_id:prodId});
+        if(!product){
+            const error=new Error('Could not find product with provided id.');
+            error.code=404;
+            throw error;
+        }
+
+        return product;
+    }
+    static async findAll(){
+        const products=await db.getDb().collection('products').find().toArray();
+        return products.map(function(productDocument){
+            return new Product(productDocument);
+        });
+    }
+    async save(){
+        const productData={
+            title:this.title,
+            summary:this.summary,
+            price:this.price,
+            description:this.description, 
+            image:this.image  
+        };
+       await db.getDb().collection('products').insertOne(productData);
+    }
+}
+
+module.exports=Product;
